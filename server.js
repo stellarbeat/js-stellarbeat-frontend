566b630b77b056123e52149f5afe1e9f78f23d2b
pieterjan84 <pieterjan84@users.noreply.github.com>
2018-10-31 10:11:30 +0100

    added compression to server

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const serveStatic = require('serve-static');
 const history = require('connect-history-api-fallback');
+const compression = require('compression');
+
 
 let port = process.env.PORT || 3000;
+app.use(compression());
 app.use(history());
 app.use("/", serveStatic ('./dist') );
 
