4319ffd8248ea6eee6659d5b2ddad75b2a5267da
pieterjan84 <pieterjan84@users.noreply.github.com>
2018-11-02 12:39:37 +0100

    server tweaks

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const compression = require('compression');
 
 let port = process.env.PORT || 3000;
 app.use(compression());
+app.use(history({
+    rewrites: [
+        { from: /\/about/, to: '/about/index.html'},
+        { from: /\/api/, to: '/api/index.html'},
+    ]
+}));
 app.use("/", serveStatic ('./dist') );
-app.use(history());
 
 app.listen(port, () => console.log('app listening on port: ' + port));
