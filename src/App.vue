6ec53109c537bf1e661d8cf198604456461f8522
pieterjan84 <pieterjan84@users.noreply.github.com>
2018-11-07 15:13:03 +0100

    design tweak

--- src/App.vue
+++ src/App.vue
@@ -15,33 +15,33 @@
         <div class="page-main">
             <b-navbar toggle-breakpoint="lg" class="m-0 p-0" toggleable="md">
                 <div class="header pt-2 pb-0 pl-4 pr-4 my-header">
-                <div class="container-fluid">
-                    <div class="d-flex">
-                        <router-link class="header-brand mt-2" :to="{ name: 'home'}" exact>
-                            <img src="./assets/logo.png" class="header-brand-img" alt="stellarbeat.io">
-                        </router-link>
-                        <div class="d-flex flex-column ml-auto">
-                            <h2 class="brand-title mb-0">stellarbeat.io</h2>
-                            <h6 class="text-muted">stellar network visibility</h6>
-                        </div>
-                        <div class="d-flex ml-auto">
-                            <div class="nav-item d-none d-lg-flex">
-                                <a href="https://github.com/stellarbeat"
-                                   class="btn btn-sm bt btn-outline-primary" target="_blank"><i
-                                        class="fe fe-github"></i> Github</a>
-                                <a href="mailto:stellarbeatio@gmail.com"
-                                   class="btn btn-sm bt btn-outline-primary ml-2" target="_blank"><i
-                                        class="fe fe-mail"></i> Mail</a>
+                    <div class="container-fluid">
+                        <div class="d-flex">
+                            <router-link class="header-brand mt-2" :to="{ name: 'home'}" exact>
+                                <img src="./assets/logo.png" class="header-brand-img" alt="stellarbeat.io">
+                            </router-link>
+                            <div class="d-flex flex-column ml-auto">
+                                <h2 class="brand-title mb-0">stellarbeat.io</h2>
+                                <h6 class="text-muted">stellar network visibility</h6>
                             </div>
-                        </div>
-                        <b-navbar-toggle class="my-navbar-toggle mb-2" target="nav_collapse"></b-navbar-toggle>
+                            <div class="d-flex ml-auto">
+                                <div class="nav-item d-none d-lg-flex">
+                                    <a href="https://github.com/stellarbeat"
+                                       class="btn btn-sm bt btn-outline-primary" target="_blank"><i
+                                            class="fe fe-github"></i> Github</a>
+                                    <a href="mailto:stellarbeatio@gmail.com"
+                                       class="btn btn-sm bt btn-outline-primary ml-2" target="_blank"><i
+                                            class="fe fe-mail"></i> Mail</a>
+                                </div>
+                            </div>
+                            <b-navbar-toggle class="my-navbar-toggle mb-2" target="nav_collapse"></b-navbar-toggle>
 
-                        <!--a href="#" class="header-toggler d-lg-none ml-3 ml-lg-0" data-toggle="collapse"
-                           data-target="#headerMenuCollapse">
-                            <span class="header-toggler-icon"></span>
-                        </a!-->
+                            <!--a href="#" class="header-toggler d-lg-none ml-3 ml-lg-0" data-toggle="collapse"
+                               data-target="#headerMenuCollapse">
+                                <span class="header-toggler-icon"></span>
+                            </a!-->
+                        </div>
                     </div>
-                </div>
                 </div>
 
             </b-navbar>
@@ -81,15 +81,18 @@
             </b-collapse>
             <div class="my-0 my-md-5 pl-4 pr-4">
                 <div class="container-fluid">
-                    <div v-if="isLoading && !isFullPreRenderRoute" class="row">
-                        <div class="col-5"></div>
-                        <div class="col-2 loader"></div>
-                        <div class="col-5"></div>
+                    <div class="my-3 my-md-5">
+                        <div v-if="isLoading && !isFullPreRenderRoute" class="row">
+                            <div class="col-5"></div>
+                            <div class="col-2 loader"></div>
+                            <div class="col-5"></div>
 
                     </div>
                     <router-view v-if="!isLoading || isFullPreRenderRoute" :network="network" :isLoading="isLoading">
 
-                    </router-view>
+                        </router-view>
+                    </div>
+
                 </div>
             </div>
         </div>
