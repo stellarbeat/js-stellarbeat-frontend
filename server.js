fe79f2e304b736679de7e2bc220b5f095879799d
pieterjan84 <pieterjan84@users.noreply.github.com>
2018-11-10 12:16:36 +0100

    removed server compression, now handled by cloudflare

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const express = require('express');
 const app = express();
 const serveStatic = require('serve-static');
 const history = require('connect-history-api-fallback');
-const compression = require('compression');
 
 
 let port = process.env.PORT || 3000;
 
-app.use(compression());
 app.use(history({
     rewrites: [
         { from: /\/about/, to: '/about/index.html'},
