83d1d489e84e0a5b08607936db2a10e3a7ab640f
pieterjan84 <pieterjan84@users.noreply.github.com>
2018-11-12 19:39:05 +0100

    graph legend design. updated manual.

--- src/components/quorum-monitor/graph/graph-legend.vue
+++ src/components/quorum-monitor/graph/graph-legend.vue
@@ -2,32 +2,54 @@
     <div class="row" style="width: 100%">
         <div class="col-12">
             <div class="row">
-                <div class="col-12 legend-link">
+                <div class="col-12 legend-link mb-2">
 
                     <a v-show="!showLegend" v-on:click.prevent.stop="showLegend=true" href="#">Show legend</a>
                     <a v-show="showLegend" v-on:click.prevent.stop="showLegend=false" href="#">Hide legend</a>
                 </div>
             </div>
-            <div v-show="showLegend" class="row text-center quorum-graph-legend">
-
-                <div class="col-2 element selected-node">
-                    Selected
-                </div>
-                <div class="col-2 element active-node">
-                    Active
-                </div>
-                <div class="col-2 element inactive-node">
-                    Inactive
+            <div v-show="showLegend" class="row quorum-graph-legend mb-2">
+                <div class="col-4 col-sm-2 d-flex">
+                    <div class="element selected-node px-2 m-2">
+                    </div>
+                    <div >
+                        Selected node
+                    </div>
                 </div>
 
-                <div class="col-2 element failing-node">
-                    Failing
+                <div class="col-4 col-sm-2  d-flex">
+                    <div class=" element active-node px-2 m-2"></div>
+                    <div >
+                        Active node
+                    </div>
                 </div>
-                <div class="col-2 element incoming-connection">
-                    Incoming Link
+
+                <div class="col-4 col-sm-2  d-flex">
+                    <div class=" element inactive-node px-2 m-2"></div>
+                    <div >
+                        Inactive node
+                    </div>
                 </div>
-                <div class="col-2 element outgoing-connection">
-                    Outgoing Link
+
+                <div class="col-4 col-sm-2  d-flex">
+                    <div class=" element failing-node px-2 m-2"></div>
+                    <div >
+                        Failing node
+                    </div>
+                </div>
+
+                <div class="col-4 col-sm-2  d-flex">
+                    <div class=" element incoming-connection px-2 m-2"></div>
+                    <div >
+                        Incoming Link
+                    </div>
+                </div>
+
+                <div class="col-4 col-sm-2  d-flex">
+                    <div class=" element outgoing-connection px-2 m-2"></div>
+                    <div >
+                        Outgoing Link
+                    </div>
                 </div>
             </div>
 
@@ -50,15 +72,11 @@
 </script>
 
 <style scoped>
-    .quorum-graph-legend {
-        margin-top: 20px;
-        font-size: 0.7rem;
-    }
-
     .quorum-graph-legend .element {
-        border-radius: 5px;
-        opacity: 0.8;
-
+        color: white;
+        border-radius: 0px;
+        width: 30px;
+        height: 10px;
     }
 
     .quorum-graph-legend .connection {
@@ -91,6 +109,5 @@
 
     .legend-link {
         font-size: 0.8rem;
-        margin-bottom: 0px;
     }
 </style>
--- src/components/quorum-monitor/manual.vue
+++ src/components/quorum-monitor/manual.vue
@@ -2,14 +2,18 @@
     <div>
         <ul>
             <li>Click and drag on the Graph to pan</li>
-            <li>Zoom the graph with the controls in the bottom right corner</li>
-            <li>Search or click on a node to select it</li>
-            <li>Click on the legend on the bottom on the graph to see the color codes</li>
+            <li>Zoom the graph in or out with the controls in the bottom right corner.</li>
+            <li>Select a node through search in the upper right corner, or by clicking on it in the graph.</li>
+            <li>Click on "show legend" on the bottom of the graph to see the color codes for the nodes & connections.
+            </li>
             <li>Links are only shown between active, non-failing nodes</li>
-            <li>Clustered nodes have fuller links and are closer together in the graph</li>
-            <li>Press the info button in the selected node details for a complete list of the node details</li>
+            <li>Clustered nodes have fuller links and are closer together in the graph. A node is part of a cluster if
+                it is connected to itself through the other nodes in the cluster.
+            </li>
+            <li>Press the info button in the selected node details for a complete list of the node details.</li>
             <li>
-                Toggle the on/off button in the selected node details to activate/deactivate the node and see the effects on the network
+                Toggle the on/off button in the selected node details to activate/deactivate the node and see the
+                effects on the network.
             </li>
         </ul>
     </div>
