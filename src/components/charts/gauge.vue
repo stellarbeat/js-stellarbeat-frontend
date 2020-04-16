7ac23690c17f674f45ccc37a56b5499fdd439203
pieterjan84 <pieterjan84@users.noreply.github.com>
2020-04-16 21:03:17 +0200

    extracted gauge chart component. added centered text

---
+++ src/components/charts/gauge.vue
@@ -0,0 +1,122 @@
+<template>
+    <div class="d-flex flex-column justify-content-around align-items-center">
+        <div class="relative">
+            <canvas :height="height" :width="width" ref="chart"/>
+            <div class="absolute-center text-center">
+                {{centerText}}
+            </div>
+        </div>
+        <div class="title">
+            {{title}}
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import {Component, Prop, Watch} from 'vue-property-decorator';
+    import Store from '@/store/Store';
+    import Chart from 'chart.js';
+
+    @Component({})
+    export default class Gauge extends Vue {
+        @Prop()
+        width!: string;
+        @Prop()
+        height!: string;
+        @Prop()
+        value!: number;
+        @Prop({default: '#1997c6'})
+        valueColor!: string;
+        @Prop({default: '#e6e5e5'})
+        negativeValueColor!: string;
+        @Prop()
+        maxValue!: number;
+        @Prop()
+        title!: string;
+        @Prop()
+        centerText!: string;
+
+        chart!: Chart;
+
+        get store(): Store {
+            return this.$root.$data.store;
+        }
+
+        @Watch('value')
+        onValueChanged() {
+            console.log('update');
+            this.chart.data.datasets![0].data = [this.value, this.maxValue - this.value];
+
+            this.chart.update();
+        }
+
+        mounted() {
+            let canvas = this.$refs.chart as HTMLCanvasElement;
+            this.chart = new Chart(canvas, {
+                type: 'doughnut',
+
+                data: {
+                    datasets: [{
+                        label: this.title,
+                        backgroundColor: [
+                            this.valueColor,
+                            this.negativeValueColor
+                        ],
+                        data: [this.value, this.maxValue - this.value],
+                    }],
+                    labels: [
+                        this.title,
+                        ''
+                    ]
+                },
+                // Configuration options go here
+                options: {
+                    legend: {
+                        display: false
+                    },
+                    tooltips: {
+                        enabled: true
+                    },
+                    hover: {mode: undefined},
+                    responsive: false,
+                    cutoutPercentage: 70,
+                    maintainAspectRatio: true
+
+                },
+            });
+        }
+
+        public beforeDestroy() {
+            if (this.chart) {
+                this.chart.destroy();
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .title {
+        font-weight: bold;
+        font-size: 16px;
+        opacity: 0.7;
+    }
+
+    .relative {
+        position: relative;
+    }
+
+    .absolute-center {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    .text-center {
+        text-align: center;
+        font-weight: bold;
+        font-size: 15px;
+        opacity: 0.6;
+    }
+</style>
--- src/components/node/node-cards/node-profile.vue
+++ src/components/node/node-cards/node-profile.vue
@@ -12,8 +12,12 @@
                 </h4>
             </div>
             <div class="d-flex flex-row flex-wrap justify-content-center">
-                    <canvas id="indexChart" width="100px" height="80px" ref="indexChart"/>
-                    <canvas id="validatingChart" width="100px" height="80px" ref="validatingChart"/>
+                    <gauge width="100px" height="100px"
+                           :value="node.index" :max-value="1" title="Index"
+                           :centerText="+node.index.toFixed(2)"/>
+                    <!--gauge value-color="#5eba00" negative-value-color="#cd201f" width="150px" height="100px" :value="node.statistics.validating24HoursPercentage" :max-value="100" title="24H Validating"
+                           :center-text="+node.statistics.validating24HoursPercentage.toFixed(2) + '%'"
+                           /!-->
             </div>
 
 
@@ -26,8 +30,11 @@
     import {Network, Node} from '@stellarbeat/js-stellar-domain';
     import Store from '@/store/Store';
     import Chart from 'chart.js';
+    import Gauge from '@/components/charts/gauge.vue';
 
-    @Component({})
+    @Component({
+        components: {Gauge}
+    })
     export default class NodeProfile extends Vue {
         @Prop()
         protected node!: Node;
@@ -39,84 +46,6 @@
 
         get network(): Network {
             return this.store.network;
-        }
-
-        mounted() {
-            const context = this.$refs.indexChart;
-            this.chart = new Chart(context as HTMLCanvasElement, {
-                type: 'doughnut',
-
-                data: {
-                    datasets: [{
-                        label: 'index',
-                        backgroundColor: [
-                            '#1997c6', // success blue
-                            'gray'
-                        ],
-                        data: [this.node.index, 1 - this.node.index],
-                    }],
-                    labels: [
-                        'Index',
-                        ''
-                    ]
-                },
-                // Configuration options go here
-                options: {
-                    title: {
-                        display: true,
-                        text: 'Index',
-                        position: 'bottom',
-                        padding: 5,
-                    },
-                    legend: {
-                        display: false
-                    },
-                    responsive: false,
-                    cutoutPercentage: 60,
-                    maintainAspectRatio: true
-
-                },
-            });
-            const context2 = this.$refs.validatingChart;
-            new Chart(context2 as HTMLCanvasElement, {
-                type: 'doughnut',
-
-                data: {
-                    datasets: [{
-                        backgroundColor: [
-                            '#5eba00', // success blue
-                            '#cd201f'
-                        ],
-                        data: [this.node.statistics.validating24HoursPercentage, 100 - this.node.statistics.validating24HoursPercentage],
-                    }],
-                    labels: [
-                        'Validating',
-                        'Failing'
-                    ]
-                },
-                // Configuration options go here
-                options: {
-                    title: {
-                        display: true,
-                        text: '24H Validating',
-                        position: 'bottom',
-                        padding: 5,
-                    },
-                    legend: {
-                        display: false
-                    },
-                    responsive: false,
-                    cutoutPercentage: 60,
-                    maintainAspectRatio: false
-
-                },
-            });
-        }
-
-        public beforeDestroy() {
-            if (this.chart) {
-                this.chart.destroy();
-            }
         }
     };
 </script>
@@ -148,6 +77,7 @@
     .social-link {
         text-decoration: none;
     }
+
     .chart-container {
         height: 70px;
         width: 140px;
