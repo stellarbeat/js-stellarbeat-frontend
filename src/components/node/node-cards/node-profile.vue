fb25ca62fbf5385515494cbfd117b09c84b8bfdb
pieterjan84 <pieterjan84@users.noreply.github.com>
2020-04-18 10:06:28 +0200

    cleaning up node profile

--- src/components/node/node-cards/node-profile.vue
+++ src/components/node/node-cards/node-profile.vue
@@ -16,9 +16,6 @@
                     <gauge width="100px" height="100px"
                            :value="node.index" :max-value="1" title="Node index"
                            :centerText="+node.index.toFixed(2)"/>
-                    <!--gauge value-color="#5eba00" negative-value-color="#cd201f" width="150px" height="100px" :value="node.statistics.validating24HoursPercentage" :max-value="100" title="24H Validating"
-                           :center-text="+node.statistics.validating24HoursPercentage.toFixed(2) + '%'"
-                           /!-->
             </div>
             <b-alert :show="this.node.historyUrl && !this.node.isFullValidator" variant="warning">
                 <b-icon-exclamation-triangle/> History archive not up-to-date
@@ -49,55 +46,9 @@
         get network(): Network {
             return this.store.network;
         }
-
-        get colorClass() {
-            return {
-                'success': !this.network.isNodeFailing(this.node),
-                'danger': this.network.isNodeFailing(this.node)
-            };
-        }
-
-        get backGroundClass() {
-            return {
-                'bg-success': !this.network.isNodeFailing(this.node),
-                'bg-danger': this.network.isNodeFailing(this.node)
-            };
-        }
     };
 </script>
 
 <style scoped>
-    li {
-        margin-bottom: 5px;
-    }
-    .danger {
-        color: #cd201f
-    }
-    .title {
-        font-size: 24px;
-        line-height: 1.2;
-        font-weight: 400;
-    }
 
-    .public-key {
-
-    }
-
-    .type {
-        font-size: 16px;
-        color: #9aa0ac;
-    }
-
-    .profile-list {
-        list-style: none;
-    }
-
-    .social-link {
-        text-decoration: none;
-    }
-
-    .chart-container {
-        height: 70px;
-        width: 140px;
-    }
 </style>
