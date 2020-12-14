96aedd75992c4be9988ba2bc58c58a0b491644a7
pieterjan84 <pieterjan84@users.noreply.github.com>
2020-12-14 13:09:38 +0100

    experimenting with lazy hydration to improve mobile loading times

--- src/components/network/network-dashboard.vue
+++ src/components/network/network-dashboard.vue
@@ -1,94 +1,126 @@
 <template>
     <div>
         <div class="row row-cards row-deck">
-            <div class="col-12">
-                <network-statistics :network="network"/>
-            </div>
-            <div class="col-lg-4 col-xl-4" v-if="!store.isSimulation">
-                <NodesCountryDistribution/>
-            </div>
-            <div class="col-lg-4 col-xl-4" v-if="!store.isSimulation">
-                <NodesVersions/>
-            </div>
-            <div class="col-lg-4 col-xl-4" v-if="!store.isSimulation">
-                <ValidatorsServerLoad/>
-            </div>
-            <div class="col-lg-6 col-xl-6" v-if="!store.isSimulation">
-                <NetworkAnalysis analysis-type="liveness" default-bucket-size="30D">
-                    <template v-slot:info>
-                        <liveness-info/>
-                    </template>
-                </NetworkAnalysis>
-            </div>
-            <div class="col-lg-6 col-xl-6" v-if="!store.isSimulation">
-                <NetworkAnalysis analysis-type="safety">
-                    <template v-slot:info>
-                        <safety-info/>
-                    </template>
-                </NetworkAnalysis>
-            </div>
-            <div v-if="network.organizations.length > 0" class="col-lg-6 col-xl-6">
-                <network-organizations/>
-            </div>
-            <div class="col-lg-6 col-xl-6">
-                <network-nodes/>
-            </div>
-            <div v-if="network.organizations.length > 0 && !store.isSimulation" class="col-lg-6 col-xl-6">
-                <network-organization-updates/>
-            </div>
-            <div class="col-lg-6 col-xl-6" v-if="!store.isSimulation">
-                <network-validator-updates/>
-            </div>
+            <Hydrate when-visible>
+                <div class="col-12">
+                    <network-statistics :network="network"/>
+                </div>
+            </Hydrate>
+            <Hydrate when-visible>
+
+                <div class="col-lg-4 col-xl-4" v-if="!store.isSimulation">
+                    <NodesCountryDistribution/>
+                </div>
+            </Hydrate>
+
+            <Hydrate when-visible>
+                <div class="col-lg-4 col-xl-4" v-if="!store.isSimulation">
+                    <NodesVersions/>
+                </div>
+            </Hydrate>
+
+            <Hydrate when-visible>
+                <div class="col-lg-4 col-xl-4" v-if="!store.isSimulation">
+                    <ValidatorsServerLoad/>
+                </div>
+            </Hydrate>
+
+            <Hydrate when-visible>
+                <div class="col-lg-6 col-xl-6" v-if="!store.isSimulation">
+                    <NetworkAnalysis analysis-type="liveness" default-bucket-size="30D">
+                        <template v-slot:info>
+                            <liveness-info/>
+                        </template>
+                    </NetworkAnalysis>
+                </div>
+            </Hydrate>
+
+            <Hydrate when-visible>
+                <div class="col-lg-6 col-xl-6" v-if="!store.isSimulation">
+                    <NetworkAnalysis analysis-type="safety">
+                        <template v-slot:info>
+                            <safety-info/>
+                        </template>
+                    </NetworkAnalysis>
+                </div>
+            </Hydrate>
+
+            <Hydrate when-visible>
+                <div v-if="network.organizations.length > 0" class="col-lg-6 col-xl-6">
+                    <network-organizations/>
+                </div>
+            </Hydrate>
+
+            <Hydrate when-visible>
+                <div class="col-lg-6 col-xl-6">
+                    <network-nodes/>
+                </div>
+            </Hydrate>
+
+            <Hydrate when-visible>
+                <div v-if="network.organizations.length > 0 && !store.isSimulation" class="col-lg-6 col-xl-6">
+                    <network-organization-updates/>
+                </div>
+            </Hydrate>
+
+            <Hydrate when-visible>
+                <div class="col-lg-6 col-xl-6" v-if="!store.isSimulation">
+                    <network-validator-updates/>
+                </div>
+            </Hydrate>
+
         </div>
     </div>
 </template>
 <script lang="ts">
-    import Vue from 'vue';
-    import {Component, Prop} from 'vue-property-decorator';
-    import NodesCountryDistribution from '@/components/network/cards/nodes-country-distribution.vue';
-    import NodesVersions from '@/components/network/cards/nodes-versions.vue';
-    import ValidatorsServerLoad from '@/components/network/cards/validator-load.vue';
-    import {Network} from '@stellarbeat/js-stellar-domain';
-    import NetworkStatistics from '@/components/network/cards/network-statistics/network-statistics.vue';
-    import NetworkNodes from '@/components/network/cards/network-nodes.vue';
-    import NetworkOrganizations from '@/components/network/cards/network-organizations.vue';
-    import NetworkAnalysis from '@/components/network/cards/network-risk-analysis-charts/network-analysis.vue';
-    import {BCard, BListGroup, BListGroupItem, BBadge} from 'bootstrap-vue';
-    import LivenessInfo from '@/components/network/cards/network-risk-analysis-charts/liveness-info.vue';
-    import SafetyInfo from '@/components/network/cards/network-risk-analysis-charts/safety-info.vue';
-    import NetworkValidatorUpdates from '@/components/network/cards/network-validator-updates.vue';
-    import NetworkOrganizationUpdates from '@/components/network/cards/network-organization-updates.vue';
-    import Store from '@/store/Store';
+import Vue from 'vue';
+import {Component, Prop} from 'vue-property-decorator';
+import NodesCountryDistribution from '@/components/network/cards/nodes-country-distribution.vue';
+import NodesVersions from '@/components/network/cards/nodes-versions.vue';
+import ValidatorsServerLoad from '@/components/network/cards/validator-load.vue';
+import {Network} from '@stellarbeat/js-stellar-domain';
+import NetworkStatistics from '@/components/network/cards/network-statistics/network-statistics.vue';
+import NetworkNodes from '@/components/network/cards/network-nodes.vue';
+import NetworkOrganizations from '@/components/network/cards/network-organizations.vue';
+import NetworkAnalysis from '@/components/network/cards/network-risk-analysis-charts/network-analysis.vue';
+import {BCard, BListGroup, BListGroupItem, BBadge} from 'bootstrap-vue';
+import LivenessInfo from '@/components/network/cards/network-risk-analysis-charts/liveness-info.vue';
+import SafetyInfo from '@/components/network/cards/network-risk-analysis-charts/safety-info.vue';
+import NetworkValidatorUpdates from '@/components/network/cards/network-validator-updates.vue';
+import NetworkOrganizationUpdates from '@/components/network/cards/network-organization-updates.vue';
+import Store from '@/store/Store';
+import Hydrate from 'lazy-hydration';
 
-    @Component({
-        components: {
-            NetworkOrganizationUpdates,
-            NetworkValidatorUpdates,
-            SafetyInfo,
-            LivenessInfo,
-            NetworkAnalysis,
-            NetworkOrganizations,
-            NetworkNodes,
-            NetworkStatistics,
-            ValidatorsServerLoad,
-            NodesVersions,
-            NodesCountryDistribution,
-            BCard,
-            BListGroup,
-            BListGroupItem,
-            BBadge
-        }
-    })
-
-    export default class NetworkDashboard extends Vue {
-        get network(): Network {
-            return this.$root.$data.store.network;
-        }
-
-        get store(): Store {
-            return this.$root.$data.store;
-        }
+@Component({
+    components: {
+        NetworkOrganizationUpdates,
+        Hydrate,
+        NetworkValidatorUpdates,
+        SafetyInfo,
+        LivenessInfo,
+        NetworkAnalysis,
+        NetworkOrganizations,
+        NetworkNodes,
+        NetworkStatistics,
+        ValidatorsServerLoad,
+        NodesVersions,
+        NodesCountryDistribution,
+        BCard,
+        BListGroup,
+        BListGroupItem,
+        BBadge
     }
+})
+
+export default class NetworkDashboard extends Vue {
+    get network(): Network {
+        return this.$root.$data.store.network;
+    }
+
+    get store(): Store {
+        return this.$root.$data.store;
+    }
+}
 </script>
 <style scoped>
 
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -11,18 +11,18 @@
         <div class="row row-cards row-deck" v-if="store.isHaltingAnalysisVisible" id="halting-analysis-card">
             <div class="col-12">
                 <HaltingAnalysis
-                        :network="network"
-                        :publicKey="store.haltingAnalysisPublicKey"
-                        v-on:update-validating-states="updateValidatingStates">
+                    :network="network"
+                    :publicKey="store.haltingAnalysisPublicKey"
+                    v-on:update-validating-states="updateValidatingStates">
                 </HaltingAnalysis>
             </div>
         </div>
-        <div class="row h-100" >
+        <div class="row h-100">
             <aside class="col-xs-12 col-sm-5 col-lg-3 col-xl-auto mb-5">
                 <div class="card pt-0 sidebar-card h-100">
                     <transition
-                            name="fade"
-                            mode="out-in"
+                        name="fade"
+                        mode="out-in"
                     >
                         <router-view name="sideBar" class="h-100 side-bar"/>
                     </transition>
@@ -31,7 +31,9 @@
             <div class="col-sm-7 col-lg-9 col-xl" id="content">
                 <div class="row">
                     <div class="col">
-                        <network-visual-navigator :view="view"/>
+                        <Hydrate when-visible>
+                            <network-visual-navigator :view="view"/>
+                        </Hydrate>
                     </div>
                 </div>
                 <div class="row" v-if="store.isNetworkAnalysisVisible" id="network-analysis-card">
@@ -40,9 +42,9 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-12">
+                    <Hydrate when-visible class="col-12">
                         <router-view name="dashboard"/>
-                    </div>
+                    </Hydrate>
                 </div>
             </div>
         </div>
@@ -51,134 +53,138 @@
 </template>
 
 <script lang="ts">
-    import Vue from 'vue';
-    import Search from '../components/search.vue';
-    import NetworkStatistics from '../components/network/cards/network-statistics/network-statistics.vue';
-    import HaltingAnalysis from '@/components/node/tools/halting-analysis/halting-analysis.vue';
-    import {Node, Network, QuorumSet, PublicKey} from '@stellarbeat/js-stellar-domain';
-    import {Component, Prop, Watch} from 'vue-property-decorator';
-    import Store from '@/store/Store';
-    import NetworkVisualNavigator from '@/components/visual-navigator/network-visual-navigator.vue';
-    import NetworkSideBar from '@/components/network/sidebar/network-side-bar.vue';
-    import CrawlTime from '@/components/crawl-time.vue';
-    import SimulationBadge from '@/components/simulation-badge.vue';
-    import TimeTravelBadge from '@/components/time-travel-badge.vue';
-    import {BBreadcrumb} from 'bootstrap-vue';
+import Vue from 'vue';
+import Search from '../components/search.vue';
+import NetworkStatistics from '../components/network/cards/network-statistics/network-statistics.vue';
+import HaltingAnalysis from '@/components/node/tools/halting-analysis/halting-analysis.vue';
+import {Node, Network, QuorumSet, PublicKey} from '@stellarbeat/js-stellar-domain';
+import {Component, Prop, Watch} from 'vue-property-decorator';
+import Store from '@/store/Store';
+import NetworkVisualNavigator from '@/components/visual-navigator/network-visual-navigator.vue';
+import NetworkSideBar from '@/components/network/sidebar/network-side-bar.vue';
+import CrawlTime from '@/components/crawl-time.vue';
+import SimulationBadge from '@/components/simulation-badge.vue';
+import TimeTravelBadge from '@/components/time-travel-badge.vue';
+import {BBreadcrumb} from 'bootstrap-vue';
+import Hydrate from 'lazy-hydration';
 
-    @Component({
-        name: 'dashboard',
-        components: {
-            'network-analysis' : () => import('@/components/network/tools/network-analysis/network-analysis.vue'),
-            TimeTravelBadge,
-            SimulationBadge,
-            CrawlTime,
-            NetworkSideBar,
-            NetworkVisualNavigator,
-            Search,
-            Statistics: NetworkStatistics,
-            HaltingAnalysis,
-            BBreadcrumb: BBreadcrumb
-        }
-    })
-    export default class Dashboard extends Vue {
-        @Prop({default: 'graph'})
-        public view!: string;
+@Component({
+    name: 'dashboard',
+    components: {
+        Hydrate,
+        'network-analysis': () => import('@/components/network/tools/network-analysis/network-analysis.vue'),
+        TimeTravelBadge,
+        SimulationBadge,
+        CrawlTime,
+        NetworkSideBar,
+        NetworkVisualNavigator,
+        Search,
+        Statistics: NetworkStatistics,
+        HaltingAnalysis,
+        BBreadcrumb: BBreadcrumb
+    }
+})
+export default class Dashboard extends Vue {
+    @Prop({default: 'graph'})
+    public view!: string;
 
-        @Watch('$route', {immediate: true})
-        public onRouteChanged(to: any) {
-            if(to.params.publicKey){
-                this.store.selectedNode = this.network.getNodeByPublicKey(to.params.publicKey);
-                if (!this.store.selectedNode) {
-                    this.$router.push(
-                        {
-                            name: 'network-dashboard', query: { view: this.$route.query.view, network: this.$route.query.network}
-                        },
-                    );
-                }
-            } else
-                this.store.selectedNode = undefined;
-            if(to.params.organizationId){
-                this.store.selectedOrganization = this.network.getOrganizationById(to.params.organizationId);
-                if (!this.store.selectedOrganization) {
-                    this.$router.push(
-                        {
-                            name: 'network-dashboard', query: { view: this.$route.query.view, network: this.$route.query.network}
-                        },
-                    );
-                }
-            } else
-                this.store.selectedOrganization = undefined;
-
-            if (to.query.center === '1' || to.query.center === undefined) {
-                this.store.centerNode = this.store.selectedNode;
+    @Watch('$route', {immediate: true})
+    public onRouteChanged(to: any) {
+        if (to.params.publicKey) {
+            this.store.selectedNode = this.network.getNodeByPublicKey(to.params.publicKey);
+            if (!this.store.selectedNode) {
+                this.$router.push(
+                    {
+                        name: 'network-dashboard',
+                        query: {view: this.$route.query.view, network: this.$route.query.network}
+                    },
+                );
             }
-        }
+        } else
+            this.store.selectedNode = undefined;
+        if (to.params.organizationId) {
+            this.store.selectedOrganization = this.network.getOrganizationById(to.params.organizationId);
+            if (!this.store.selectedOrganization) {
+                this.$router.push(
+                    {
+                        name: 'network-dashboard',
+                        query: {view: this.$route.query.view, network: this.$route.query.network}
+                    },
+                );
+            }
+        } else
+            this.store.selectedOrganization = undefined;
 
-        @Watch('store.haltingAnalysisPublicKey')
-        public onHaltingAnalysisPublicKeyChanged(publicKey?: string) {
-            if (publicKey)
-                this.$nextTick(() => {
-                    this.$scrollTo('#halting-analysis-card');
-                });
-        }
-
-        get breadCrumbs() {
-            let crumbs = [];
-            crumbs.push({
-                text: this.store.getNetworkIdPretty(),
-                to: {name: 'network-dashboard', query: { view: this.$route.query.view, network: this.$route.query.network}}
-            });
-
-            if (this.selectedNode) {
-                if (this.selectedNode.organizationId)
-                    crumbs.push({
-                        text: this.network.getOrganizationById(this.selectedNode.organizationId)!.name,
-                        to: {name: 'organization-dashboard', params: { 'organizationId': this.selectedNode.organizationId}},
-                        active: false
-                    });
-                crumbs.push({
-                    text: this.selectedNode.displayName,
-                    active: true
-                });
-            } else if (this.selectedOrganization)
-                crumbs.push({
-                    text: this.selectedOrganization.name,
-                    active: true
-                });
-            return crumbs;
-        }
-
-        get store(): Store {
-            return this.$root.$data.store;
-        }
-
-        get selectedNode() {
-            return this.store.selectedNode;
-        }
-
-        get selectedOrganization() {
-            return this.store.selectedOrganization;
-        }
-
-        get network(): Network {
-            return this.$root.$data.store.network;
-        }
-
-        public updateValidatingStates(updates: Array<{ 'publicKey': PublicKey, 'validating': boolean }>) {
-            this.store.updateValidatingStates(updates);
-        }
-
-        get isSimulation(): boolean {
-            return this.store.isSimulation;
+        if (to.query.center === '1' || to.query.center === undefined) {
+            this.store.centerNode = this.store.selectedNode;
         }
     }
+
+    @Watch('store.haltingAnalysisPublicKey')
+    public onHaltingAnalysisPublicKeyChanged(publicKey?: string) {
+        if (publicKey)
+            this.$nextTick(() => {
+                this.$scrollTo('#halting-analysis-card');
+            });
+    }
+
+    get breadCrumbs() {
+        let crumbs = [];
+        crumbs.push({
+            text: this.store.getNetworkIdPretty(),
+            to: {name: 'network-dashboard', query: {view: this.$route.query.view, network: this.$route.query.network}}
+        });
+
+        if (this.selectedNode) {
+            if (this.selectedNode.organizationId)
+                crumbs.push({
+                    text: this.network.getOrganizationById(this.selectedNode.organizationId)!.name,
+                    to: {name: 'organization-dashboard', params: {'organizationId': this.selectedNode.organizationId}},
+                    active: false
+                });
+            crumbs.push({
+                text: this.selectedNode.displayName,
+                active: true
+            });
+        } else if (this.selectedOrganization)
+            crumbs.push({
+                text: this.selectedOrganization.name,
+                active: true
+            });
+        return crumbs;
+    }
+
+    get store(): Store {
+        return this.$root.$data.store;
+    }
+
+    get selectedNode() {
+        return this.store.selectedNode;
+    }
+
+    get selectedOrganization() {
+        return this.store.selectedOrganization;
+    }
+
+    get network(): Network {
+        return this.$root.$data.store.network;
+    }
+
+    public updateValidatingStates(updates: Array<{ 'publicKey': PublicKey, 'validating': boolean }>) {
+        this.store.updateValidatingStates(updates);
+    }
+
+    get isSimulation(): boolean {
+        return this.store.isSimulation;
+    }
+}
 </script>
 
 <style scoped>
 
-    @media (min-width: 1279px) {
-        .side-bar {
-            width: 272px;
-        }
+@media (min-width: 1279px) {
+    .side-bar {
+        width: 272px;
     }
+}
 </style>
