8ddc11793aa8f3cd6f46398e83a4046a1ef2e162
pieterjan84 <pieterjan84@users.noreply.github.com>
2020-12-15 09:32:28 +0100

    implemented lazy hydration for organization dashboard

--- src/components/organization/organization-dashboard.vue
+++ src/components/organization/organization-dashboard.vue
@@ -2,32 +2,48 @@
     <div>
         <div v-if="organization &&!store.isSimulation">
             <div class="row row-cards row-deck" v-if="!store.isSimulation">
-                <div class="col-md-12 col-lg-6 col-xl-4">
-                    <organization-profile :organization="organization"/>
-                </div>
-                <div class="col-md-12 col-lg-6 col-xl-4" v-if="!store.isSimulation">
-                    <OrganizationStatisticsSubQuorum24hAvailability :organization="organization"/>
-                    <OrganizationStatisticsSubQuorum30DAvailability :organization="organization"/>
-                </div>
-                <div class="col-md-12 col-lg-12 col-xl-4" v-if="!store.isSimulation">
-                    <history-card
-                        :subject="'Availability history'"
-                        :entityId="organization.id"
-                        :fetchDayMeasurements="(organizationId, from, to) => store.organizationMeasurementStore.getDayStatistics(organizationId, from, to)"
-                        :fetchMeasurements="(organizationId, from, to) => store.organizationMeasurementStore.getStatistics(organizationId, from, to)"
-                        :dayMeasurementProperty="'isSubQuorumAvailableCount'"
-                        :measurementProperty="'isSubQuorumAvailable'"
-                    >
-                    </history-card>
-                </div>
+                <LazyHydrate when-visible>
+
+                    <div class="col-md-12 col-lg-6 col-xl-4">
+                        <organization-profile :organization="organization"/>
+                    </div>
+                </LazyHydrate>
+                <LazyHydrate when-visible>
+
+                    <div class="col-md-12 col-lg-6 col-xl-4" v-if="!store.isSimulation">
+                        <OrganizationStatisticsSubQuorum24hAvailability :organization="organization"/>
+                        <OrganizationStatisticsSubQuorum30DAvailability :organization="organization"/>
+                    </div>
+                </LazyHydrate>
+                <LazyHydrate when-visible>
+
+                    <div class="col-md-12 col-lg-12 col-xl-4" v-if="!store.isSimulation">
+                        <history-card
+                            :subject="'Availability history'"
+                            :entityId="organization.id"
+                            :fetchDayMeasurements="(organizationId, from, to) => store.organizationMeasurementStore.getDayStatistics(organizationId, from, to)"
+                            :fetchMeasurements="(organizationId, from, to) => store.organizationMeasurementStore.getStatistics(organizationId, from, to)"
+                            :dayMeasurementProperty="'isSubQuorumAvailableCount'"
+                            :measurementProperty="'isSubQuorumAvailable'"
+                        >
+                        </history-card>
+                    </div>
+                </LazyHydrate>
             </div>
+
             <div class="row row-cards">
-                <div class="col-lg-12 col-xl-12">
-                    <organization-validators :organization="organization"/>
-                </div>
-                <div class="col-lg-12 col-xl-12" v-if="!store.isSimulation">
-                    <organization-latest-updates :organization="organization"/>
-                </div>
+                <LazyHydrate when-visible>
+
+                    <div class="col-lg-12 col-xl-12">
+                        <organization-validators :organization="organization"/>
+                    </div>
+                </LazyHydrate>
+                <LazyHydrate when-visible>
+
+                    <div class="col-lg-12 col-xl-12" v-if="!store.isSimulation">
+                        <organization-latest-updates :organization="organization"/>
+                    </div>
+                </LazyHydrate>
             </div>
         </div>
     </div>
@@ -49,9 +65,10 @@ import OrganizationStatisticsSubQuorum30DAvailability
     from '@/components/organization/organization-cards/statistics/organization-statistics-subquorum-30D-availability.vue';
 import OrganizationLatestUpdates
     from '@/components/organization/organization-cards/organization-latest-updates.vue';
-
+import LazyHydrate from 'vue-lazy-hydration';
 @Component({
     components: {
+        LazyHydrate,
         OrganizationLatestUpdates,
         OrganizationStatisticsSubQuorum30DAvailability,
         OrganizationStatisticsSubQuorum24hAvailability,
