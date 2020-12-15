6aa4c7b94e77e710a6ba6d6ac7416947ec7f5d3d
pieterjan84 <pieterjan84@users.noreply.github.com>
2020-12-15 13:16:28 +0100

    testing cache

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ server.get("*", async (req, res) => {
         }
 
         let html = await renderer.renderToString({url: req.url});
-        microCache.set(req.url, html);
+        //microCache.set(req.url, html);
         res.type('text/html');
         res.setHeader('Cache-Control', 'public, max-age=30');
         res.end(html);
