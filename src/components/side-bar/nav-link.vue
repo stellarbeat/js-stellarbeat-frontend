30921afb24eed9729c1e7428776e8f48a3e7fff6
pieterjan84 <pieterjan84@users.noreply.github.com>
2020-12-16 15:17:03 +0100

    functional component tryout for performance improvements

--- src/components/side-bar/nav-link.vue
+++ src/components/side-bar/nav-link.vue
@@ -1,39 +1,41 @@
-<template>
-    <div v-on:click="$emit('click')"
-         v-on:keyup.enter="$emit('click')"
-         v-on:keyup.space="$emit('click')"
-         :class="classObject"
-         v-on:mouseenter="hover=true"
-         v-on:mouseleave="hover=false"
+<template functional>
+    <div v-on:click="listeners.click()"
+         :class="{
+                'sb-nav-dropdown-toggle': false,
+                'sb-nav-link': props.isLinkInDropdown,
+                'sb-nav-dropdown-link': props.isLinkInDropdown
+            }"
          role="button"
          tabindex="0"
-         class="d-flex justify-content-between"
+         class="d-flex justify-content-between trigger"
     >
-        <div class="w-100 d-flex flex-row justify-content-between align-items-center">
-            <div class="w-100 d-flex flex-column align-items-stretch">
+        <div class="w-100 d-flex flex-row justify-content-between align-items-center trigger">
+            <div class="w-100 d-flex flex-column align-items-stretch trigger">
                 <div class="w-100 d-flex align-items-center align-content-center">
                     <div class="sb-nav-link-icon align-content-center">
-                        <b-icon :icon="chevronDirection" scale="0.8" v-if="showDropdownToggle"/>
-                        <b-icon v-else-if="showIcon" :icon="icon" scale="0.8"/>
+                        <b-icon :icon="$options.methods.chevronDirection(props.dropDownShowing)" scale="0.8"
+                                v-if="props.showDropdownToggle"/>
+                        <b-icon v-else-if="props.showIcon" :icon="props.icon" scale="0.8"/>
                     </div>
 
                     <div class="w-100 d-flex justify-content-between align-content-center">
                         <nav-title
-                                :title="title" :classes="'w-100 pb-0 m-height ' + (secondary ? 'secondary':'')"
-                                :has-warnings="hasWarnings"
-                                :warnings="warnings"
-                                :has-danger="hasDanger"
-                                :danger="dangers"
+                            :title="props.title" :classes="'w-100 pb-0 m-height ' + (props.secondary ? 'secondary':'')"
+                            :has-warnings="props.hasWarnings"
+                            :warnings="props.warnings"
+                            :has-danger="props.hasDanger"
+                            :danger="props.dangers"
                         />
                     </div>
                 </div>
-                <div v-if="showSubTitle" class="text-muted sub-title">
-                    {{subTitle}}
+                <div v-if="props.showSubTitle" class="text-muted sub-title">
+                    {{ props.subTitle }}
                 </div>
             </div>
             <div class="action mr-1">
                 <transition name="fade">
-                    <div :class="dropdownClass" v-show="hover">
+
+                <div :class="$options.methods.dropdownClass(props.showDropdownToggle)" class="my-hidden">
                         <slot name="action-dropdown"/>
                     </div>
                 </transition>
@@ -42,188 +44,211 @@
     </div>
 </template>
 <script lang="ts">
-    import Vue from 'vue';
-    import {Component, Prop} from 'vue-property-decorator';
-    import NavTitle from '@/components/side-bar/nav-title.vue';
-    import {
-        BIcon,
-        BIconPlus,
-        BIconDownload,
-        BIconChevronRight,
-        BIconChevronDown,
-        BIconLightning,
-        BIconLightningFill,
-        BIconGearWide,
-        BIconGear,
-        BIconSearch,
-        BBadge
-    } from 'bootstrap-vue';
+import Vue from 'vue';
+import NavTitle from '@/components/side-bar/nav-title.vue';
+import {
+    BIcon,
+    BIconPlus,
+    BIconDownload,
+    BIconChevronRight,
+    BIconChevronDown,
+    BIconLightning,
+    BIconLightningFill,
+    BIconGearWide,
+    BIconGear,
+    BIconSearch,
+    BBadge
+} from 'bootstrap-vue';
 
-    @Component({
-        components: {
-            NavTitle, BIcon, BIconPlus, BIconDownload, BIconChevronRight, BIconChevronDown, BIconLightning, BIconGear, BIconSearch,
-            BIconLightningFill,
-            BIconGearWide, BBadge
+Vue.component('nav-title', NavTitle);
+Vue.component('b-icon', BIcon);
+Vue.component('b-icon-plus', BIconPlus);
+Vue.component('b-icon-download', BIconDownload);
+Vue.component('b-icon-chevron-right', BIconChevronRight);
+Vue.component('b-icon-chevron-down', BIconChevronDown);
+Vue.component('b-icon-lightning', BIconLightning);
+Vue.component('b-icon-gear', BIconGear);
+Vue.component('b-icon-search', BIconSearch);
+Vue.component('b-icon-lightning-fill', BIconLightningFill);
+Vue.component('b-icon-gear-wide', BIconGearWide);
+Vue.component('b-badge', BBadge);
+
+export default {
+    props: {
+        title: {
+            type: String,
+            default: ''
+        },
+        showSubTitle: {
+            type: Boolean,
+            default: false
+        },
+        subTitle: {
+            type: String,
+            default: ''
+        },
+        showDropdownToggle: {
+            type: Boolean,
+            default: false
+        },
+        dropDownShowing: {
+            type: Boolean,
+            default: false
+        },
+        isLinkInDropdown: {
+            type: Boolean,
+            default: false
+        },
+        hasWarnings: {
+            type: Boolean,
+            default: false
+        },
+        warnings: {
+            type: String,
+            default: ''
+        },
+        hasDanger: {
+            type: Boolean,
+            default: false
+        },
+        dangers: {
+            type: String,
+            default: ''
+        },
+        showIcon: {
+            type: Boolean,
+            default: false
+        },
+        icon: {
+            type: String,
+            default: ''
+        },
+        secondary: {
+            type: Boolean,
+            default: false
         }
-    })
-    export default class NavLink extends Vue {
-        @Prop()
-        title!: string;
-
-        @Prop({default: false})
-        showSubTitle!: boolean;
-
-        @Prop()
-        subTitle!: string;
-
-        @Prop({default: false})
-        showDropdownToggle!: boolean;
-
-        @Prop({default: false})
-        dropDownShowing!: boolean;
-
-        @Prop({default: false})
-        isLinkInDropdown!: boolean;
-
-        @Prop({default: false})
-        hasWarnings!: boolean;
-
-        @Prop()
-        warnings!: string;
-
-        @Prop({default: false})
-        hasDanger!: boolean;
-
-        @Prop()
-        dangers!: string;
-
-        @Prop({default: false})
-        showIcon!: boolean;
-
-        @Prop({})
-        icon!: string;
-
-        @Prop({default: false})
-        secondary!: boolean;
-
-        hover: boolean = false;
-
-        get titleClass(): any {
+    },
+    methods: {
+        getTitleClass(secondary: boolean): any {
             return {
-                'secondary': this.secondary
+                'secondary': secondary
             };
-        }
-
-        get chevronDirection(): any {
-            if (this.dropDownShowing)
+        },
+        chevronDirection(dropDownShowing: boolean): any {
+            if (dropDownShowing)
                 return 'chevron-down';
             else return 'chevron-right';
-        }
-
-        get classObject(): any {
+        },
+        classObject(): any {
+            return;
+        },
+        dropdownClass(showDropdownToggle: boolean): any {
             return {
-                'sb-nav-dropdown-toggle': false,
-                'sb-nav-link': !this.isLinkInDropdown,
-                'sb-nav-dropdown-link': this.isLinkInDropdown
-            };
-        }
-
-        get dropdownClass(): any {
-            return {
-                'right-end': !this.showDropdownToggle,
-                'right': this.showDropdownToggle
+                'right-end': showDropdownToggle,
+                'right': showDropdownToggle
             };
         }
     }
+};
+
 </script>
 <style scoped>
-    .action {
-        min-width: 20px;
-    }
+.action {
+    min-width: 20px;
+}
 
-    .sub-title {
-        margin-left: 20px;
-        font-size: 12px;
-        margin-top: -5px;
-    }
+.sub-title {
+    margin-left: 20px;
+    font-size: 12px;
+    margin-top: -5px;
+}
 
-    .sb-alert {
-        color: orange;
-        position: relative;
-        left: 0px;
-        top: 0px;
-    }
+.sb-alert {
+    color: orange;
+    position: relative;
+    left: 0px;
+    top: 0px;
+}
 
-    .sb-nav-link {
-        padding: 1px 4px 1px 4px;
-        display: flex;
-        align-items: center;
-        cursor: pointer;
-        color: rgba(0, 0, 0, .55);
-        font-weight: 600;
-        outline: 0;
+.sb-nav-link {
+    padding: 1px 4px 1px 4px;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    color: rgba(0, 0, 0, .55);
+    font-weight: 600;
+    outline: 0;
 
-    }
+}
 
-    .sb-nav-link:hover {
-        background-color: #f8f9fa;
-    }
+.sb-nav-link:hover {
+    background-color: #f8f9fa;
+}
 
-    .sb-nav-link-icon {
-        font-size: 1rem;
-        width: 1rem;
-        height: 1rem;
-        display: block;
-        line-height: .99;
-        margin-right: 5px;
-        min-width: 10px;
-        opacity: 0.6;
-    }
+.sb-nav-link-icon {
+    font-size: 1rem;
+    width: 1rem;
+    height: 1rem;
+    display: block;
+    line-height: .99;
+    margin-right: 5px;
+    min-width: 10px;
+    opacity: 0.6;
+}
 
-    .sb-nav-dropdown-toggle {
-        margin-bottom: 0px;
-    }
+.sb-nav-dropdown-toggle {
+    margin-bottom: 0px;
+}
 
-    .sb-nav-dropdown-toggle::after {
-        content: "";
-        display: inline-block;
-        vertical-align: .272em;
-        width: .32em;
-        height: .32em;
-        border-bottom: 1px solid;
-        border-bottom-color: currentcolor;
-        border-bottom-style: solid;
-        border-bottom-width: 1px;
-        border-left: 1px solid;
-        border-left-color: currentcolor;
-        border-left-style: solid;
-        border-left-width: 1px;
-        transform: rotate(-45deg);
-        margin-left: auto;
-        margin-right: 5px;
-    }
+.sb-nav-dropdown-toggle::after {
+    content: "";
+    display: inline-block;
+    vertical-align: .272em;
+    width: .32em;
+    height: .32em;
+    border-bottom: 1px solid;
+    border-bottom-color: currentcolor;
+    border-bottom-style: solid;
+    border-bottom-width: 1px;
+    border-left: 1px solid;
+    border-left-color: currentcolor;
+    border-left-style: solid;
+    border-left-width: 1px;
+    transform: rotate(-45deg);
+    margin-left: auto;
+    margin-right: 5px;
+}
 
-    .fade-enter-active, .fade-leave-active {
-        transition: opacity 0.7s ease;
-    }
+.fade-enter-active, .fade-leave-active {
+    transition: opacity 0.7s ease;
+}
 
-    .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */
-    {
-        opacity: 0;
-    }
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */
+{
+    opacity: 0;
+}
 
-    .sb-nav-dropdown-link {
-        display: flex;
-        align-items: center;
-        cursor: pointer;
-        color: rgba(0, 0, 0, .55);
-        width: 100%;
-        font-weight: 400;
-        font-size: 90%;
-        padding: 1px 4px 1px 4px;
-    }
+.sb-nav-dropdown-link {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    color: rgba(0, 0, 0, .55);
+    width: 100%;
+    font-weight: 400;
+    font-size: 90%;
+    padding: 1px 4px 1px 4px;
+}
 
-    .sb-nav-dropdown-link:hover {
-        background-color: #f8f9fa;
-    }
+.sb-nav-dropdown-link:hover {
+    background-color: #f8f9fa;
+}
+
+.my-hidden {
+   opacity: 0;
+}
+
+.trigger:hover .my-hidden {
+    transition: all 0.5s ease-in-out;
+    opacity: 1;
+}
 </style>
