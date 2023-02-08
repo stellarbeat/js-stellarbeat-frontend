5f70ba6466a331aed37d6fe4f348a31b69ca75c0
pieterjan84 <pieterjan84@users.noreply.github.com>
2023-02-08 14:51:25 +0100

    organization validators to composition api

--- src/components/organization/organization-cards/organization-validators.vue
+++ src/components/organization/organization-cards/organization-validators.vue
@@ -3,61 +3,50 @@
     <nodes-table :nodes="validators" :fields="fields" />
   </div>
 </template>
-<script lang="ts">
-import { Component, Prop } from "vue-property-decorator";
-import Vue from "vue";
-import { Network, Organization } from "@stellarbeat/js-stellar-domain";
-import Store from "@/store/Store";
+<script setup lang="ts">
+import { computed } from "vue";
+import { Organization } from "@stellarbeat/js-stellar-domain";
 import NodesTable from "@/components/node/nodes-table.vue";
 import useStore from "@/store/useStore";
 
-@Component({
-  components: { NodesTable },
-})
-export default class OrganizationValidators extends Vue {
-  @Prop()
-  organization!: Organization;
-  get fields() {
-    let fields = [{ key: "name", label: "Validator" }, "country", "isp"];
-    if (!this.store.isSimulation) {
-      fields.push(
-        "index",
-        { key: "validating24Hour", label: "24H validating" },
-        { key: "validating30Days", label: "30D validating" },
-        "version"
-      );
-    }
-    return fields;
-  }
+const props = defineProps<{
+  organization: Organization;
+}>();
 
-  get store(): Store {
-    return useStore();
-  }
+const store = useStore();
 
-  get network(): Network {
-    return this.store.network;
+const fields = computed(() => {
+  let fields = [{ key: "name", label: "Validator" }, "country", "isp"];
+  if (!store.isSimulation) {
+    fields.push(
+      "index",
+      { key: "validating24Hour", label: "24H validating" },
+      { key: "validating30Days", label: "30D validating" },
+      "version"
+    );
   }
+  return fields;
+});
 
-  get validators() {
-    return this.organization.validators
-      .map((publicKey) => this.network.getNodeByPublicKey(publicKey))
-      .map((validator) => {
-        return {
-          isFullValidator: validator.isFullValidator,
-          name: validator.displayName,
-          version: validator.versionStr,
-          index: validator.index,
-          validating24Hour: validator.statistics.has24HourStats
-            ? validator.statistics.validating24HoursPercentage + "%"
-            : "NA",
-          validating30Days: validator.statistics.has30DayStats
-            ? validator.statistics.validating30DaysPercentage + "%"
-            : "NA",
-          country: validator.geoData.countryName,
-          isp: validator.isp,
-          publicKey: validator.publicKey,
-        };
-      });
-  }
-}
+const validators = computed(() => {
+  return props.organization.validators
+    .map((publicKey) => store.network.getNodeByPublicKey(publicKey))
+    .map((validator) => {
+      return {
+        isFullValidator: validator.isFullValidator,
+        name: validator.displayName,
+        version: validator.versionStr,
+        index: validator.index,
+        validating24Hour: validator.statistics.has24HourStats
+          ? validator.statistics.validating24HoursPercentage + "%"
+          : "NA",
+        validating30Days: validator.statistics.has30DayStats
+          ? validator.statistics.validating30DaysPercentage + "%"
+          : "NA",
+        country: validator.geoData.countryName,
+        isp: validator.isp,
+        publicKey: validator.publicKey,
+      };
+    });
+});
 </script>
